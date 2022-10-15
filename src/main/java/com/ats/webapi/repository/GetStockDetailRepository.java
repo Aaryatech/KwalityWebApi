a9f486626de7845a0d4b981618e12f89505bb966
vivekgosavi24 <vivekgosavi16@gmail.com>
2022-10-15 16:51:36 +0530

    //vivek

--- src/main/java/com/ats/webapi/repository/GetStockDetailRepository.java
+++ src/main/java/com/ats/webapi/repository/GetStockDetailRepository.java
@@ -12,22 +12,56 @@ import com.ats.webapi.entity.GetStockDetails;
 public interface GetStockDetailRepository extends JpaRepository<GetStockDetails,String>,
 JpaSpecificationExecutor<GetStockDetails> {
 
-	@Query(value="select UUID() as id, a.item_name,a.rol,ifnull(c.opening_quantity,0) as opening_quantity,ifnull(c.approved_quantity,0) as approved_quantity,\n" + 
-			"ifnull(d.item_issue,0) as issue_quantity\n" + 
-			"from\n" + 
-			"(select * from item) as a\n" + 
-			"LEFT JOIN\n" + 
-			"(select md.id as id,osd.opening_quantity as opening_quantity,sum(md.approved_qty) as approved_quantity,md.item_id\n" + 
-			"from opening_stock_header osh,opening_stock_detail osd,\n" + 
-			"mrn_header mh, mrn_detail md\n" + 
-			"where osh.id=osd.opening_stock_header_id and mh.id=md.mrn_id and\n" + 
-			"osd.item_id=md.item_id and mh.mrn_date between osh.from_date and :strDate and osh.status=0 \n" + 
-			"group by osd.item_id,md.item_id) as c\n" + 
-			"on c.item_id=a.id\n" + 
-			"LEFT JOIN\n" + 
-			"(select mrn_detail_id as id,sum(item_issue_qty) as item_issue\n" + 
-			"from item_issue_detail group by item_id) as d\n" + 
-			"on d.id=c.id",nativeQuery=true)
+	@Query(value=" select\n" + 
+			"        UUID() as id,\n" + 
+			"        a.item_name,\n" + 
+			"        a.rol,\n" + 
+			"        ifnull(c.opening_quantity,\n" + 
+			"        0) as opening_quantity,\n" + 
+			"        ifnull(c.approved_quantity,\n" + 
+			"        0) as approved_quantity,\n" + 
+			"        ifnull(d.item_issue,\n" + 
+			"        0) as issue_quantity ,\n" + 
+			"        ifnull(((c.opening_quantity+c.approved_quantity)-d.item_issue),0) as closing_quantity\n" + 
+			"    from\n" + 
+			"        (select\n" + 
+			"            * \n" + 
+			"        from\n" + 
+			"            item) as a \n" + 
+			"    LEFT JOIN\n" + 
+			"        (\n" + 
+			"            select\n" + 
+			"                md.id as id,\n" + 
+			"                osd.opening_quantity as opening_quantity,\n" + 
+			"                sum(md.approved_qty) as approved_quantity,\n" + 
+			"                md.item_id \n" + 
+			"            from\n" + 
+			"                opening_stock_header osh,\n" + 
+			"                opening_stock_detail osd,\n" + 
+			"                mrn_header mh,\n" + 
+			"                mrn_detail md \n" + 
+			"            where\n" + 
+			"                osh.id=osd.opening_stock_header_id \n" + 
+			"                and mh.id=md.mrn_id \n" + 
+			"                and osd.item_id=md.item_id \n" + 
+			"                and mh.mrn_date between osh.from_date and :strDate \n" + 
+			"                and osh.status=0  \n" + 
+			"            group by\n" + 
+			"                osd.item_id,\n" + 
+			"                md.item_id\n" + 
+			"        ) as c \n" + 
+			"            on c.item_id=a.id \n" + 
+			"    LEFT JOIN\n" + 
+			"        (\n" + 
+			"            select\n" + 
+			"                mrn_detail_id as id,\n" + 
+			"                sum(item_issue_qty) as item_issue \n" + 
+			"            from\n" + 
+			"                item_issue_detail \n" + 
+			"            group by\n" + 
+			"                item_id\n" + 
+			"        ) as d \n" + 
+			"            on d.id=c.id",nativeQuery=true)
 	List<GetStockDetails> getStockDetails(@Param("strDate")String strDate);
 
 	
